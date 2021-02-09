f55efbacc6b8b98166f9a1e0dd985f4bdd262feb
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-02-09 14:01:19 +0000

    Update install requirements

--- setup.py
+++ setup.py
@@ -66,12 +66,7 @@ setup(
     install_requires=[
         # Dependencies go here!
         'numpy>=1.8',
-        'dash>=1.18',
-        'dash_bootstrap_components',
-        'dash_daq',
-        'dash_defer_js_import',
         'pandas',
-        'plotly',
         'scipy'
     ],
     extras_require={
