0bb3e5fc8c77bdb95ffdb8ab40c973978aa84c54
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-02-09 11:44:28 +0000

    Update description

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # Version
     version=get_version(),
 
-    description='This is a one-week project in which we are using branching processes to estimate the time-dependent reproduction number of a disease.',  # noqa
+    description='An implementation of a Sequential Gibbs Sampling algorithm.',  # noqa
 
     long_description=get_readme(),
 
