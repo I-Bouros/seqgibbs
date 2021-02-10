119002a72c287a5e5f38d2773f289ffb01ecbeff
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-02-10 22:34:36 +0000

    Initial commit for test class

---
+++ seqgibbs/tests/test_samplers.py
@@ -0,0 +1,62 @@
+#
+# This file is part of SEQGIBBS
+# (https://github.com/I-Bouros/seqgibbs.git) which is released
+# under the MIT license. See accompanying LICENSE.md for copyright
+# notice and full license details.
+#
+
+import unittest
+
+import scipy.stats
+import numpy as np
+import numpy.testing as npt
+
+import seqgibbs as gibbs
+
+
+def fun(x):
+    """
+    Function returning the parameters of the normal sampler.
+        mean = sum of elements of x
+        variance = product of the elemets of x.
+    """
+    return sum(x), np.prod(x)
+
+
+class TestSysGibbsAlgoClass(unittest.TestCase):
+    """
+    Test the 'SysGibbsAlgo' class.
+    """
+    def test__init__(self):
+        gibbs.SysGibbsAlgo(scipy.stats.norm.rvs, sum)
+
+        with self.assertRaises(TypeError):
+            gibbs.OneDimSampler(0, sum)
+
+        with self.assertRaises(TypeError):
+            gibbs.OneDimSampler(scipy.stats.norm.rvs, 0)
+
+    def test_sample(self):
+        sampler = gibbs.OneDimSampler(scipy.stats.norm.rvs, fun)
+        current_state = np.array([1, 2, 0])
+        loc_update = 3
+
+        new_state = sampler.sample(current_state, loc_update)
+        conditional_part_of_state = np.array([
+            x for i, x in enumerate(
+                new_state.tolist()) if i != (loc_update-1)])
+
+        self.assertEqual(len(new_state), len(current_state))
+        npt.assert_array_equal(conditional_part_of_state, np.array([1, 2]))
+
+        with self.assertRaises(ValueError):
+            sampler.sample(np.array([[0, 0], [0, 0]]), 1)
+
+        with self.assertRaises(ValueError):
+            sampler.sample(np.array([[0, 0], [0, 0]]), 3)
+
+        with self.assertRaises(ValueError):
+            sampler.sample(np.array([[0, 0], [0, 0]]), '1')
+
+        with self.assertRaises(ValueError):
+            sampler.sample(np.array([[0, 0], [0, 0]]), 0)
